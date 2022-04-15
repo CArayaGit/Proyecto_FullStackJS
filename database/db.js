cdb3bb95df551fcee78653c68e0e3022b5d4d2ac
CArayaGit <c.araya.aliaga@gmail.com>
2022-04-14 22:04:51 -0400

    Avance 3, test mapas npm Cesium

--- database/db.js
+++ database/db.js
@@ -36,28 +36,32 @@ const getUsersDB = async () => {
     };
 
 
-const getSalaDB = async () => {
+const getSalaDB = async (id_sala) => {
     const client = await pool.connect();
-            try {
-                const respuesta = await client.query(
-                    "SELECT nombre_sala, ubicacion_map FROM salas WHERE id_ubicacion=$1"
-                );
-                return {
-                    ok: true,
-                    users: respuesta.rows,
-                };
-            } catch (error) {
-                console.log(error);
-                return {
+    const respuesta = await client.query(
+        "SELECT nombre_sala, ubicacion_map FROM salas WHERE id_ubicacion=$1",
+        values: [id_sala],
+    );
+    
+    try {
+        const respuesta = await client.query(query);
+        return {
+            ok: true,
+            user: respuesta.rows[0],
+        };
+        } catch (error) {
+            console.log(error);
+            return {
                     ok: false,
                     msg: error.message,
-                };
-            } finally {
+            };
+        } finally {
                 client.release();
-            }
-        };
+        }
+    };
+
 
 module.exports = {
     getUsersDB,
-    
+    getSalaDB
 };
