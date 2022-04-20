c0d9810021d8d9bdc653db2673250f837ad3798a
CArayaGit <c.araya.aliaga@gmail.com>
2022-04-19 22:09:17 -0400

    Agregando rutas y formato html

--- routes/vistas.route.js
+++ routes/vistas.route.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => res.render("index"));
+router.get("/salas", (req, res) => res.render("salas"));
+router.get("/maps", (req, res) => res.render("maps"));
 
 
 module.exports = router;
