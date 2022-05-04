f63f794830374ca0ff422271746d17e57f6a1c47
CArayaGit <c.araya.aliaga@gmail.com>
2022-05-03 21:32:10 -0400

    CRUD usuarios OK

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -107,9 +107,12 @@ const loginUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-    
+    console.log('update')
     try{
+        
         const { nombre, email, password } = req.body
+        console.log(nombre, email, password)
+
         const salt = await bcrypt.genSalt(10);
         const hashPassword = await bcrypt.hash(password, salt);
 
--- routes/vistas.route.js
+++ routes/vistas.route.js
@@ -6,7 +6,8 @@ router.get("/salas", (req, res) => res.render("salas"));
 router.get("/selecSala", (req, res) => res.render("selecSala"));
 router.get("/editUser", (req, res) => res.render("editUser"));
 router.get("/editUserB", (req, res) => res.render("editUserB"));
-router.get("/maps", (req, res) => res.render("maps"));
+router.get("/eventos", (req, res) => res.render("eventos"));
+//router.get("/maps", (req, res) => res.render("maps"));
 
 
 module.exports = router;
